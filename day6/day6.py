8e2d43df6320816ea9e18ca7aab14d2769cbaea5
Sisu Suominen <sisu.suominen@tuni.fi>
2022-12-06 11:27:47 +0200

    Day six solved

---
+++ day6/day6.py
@@ -0,0 +1,25 @@
+
+from collections import Counter
+
+file = open("day6_input.txt")
+input = file.read()
+test_input = "nppdvjthqldpwncqszvftbrmjlhg"
+def main():
+
+    found = False
+    for index in range(len(input)+1):
+        if index >= 13 and not found:
+            group_of_four = input[index-14:index]
+            unique_chars = ""
+            for char in group_of_four:
+                if char not in unique_chars:
+                    unique_chars += char
+                    print(unique_chars)
+
+                if len(unique_chars) == 14:
+                    print(index)
+                    found = True
+                else:
+                    pass
+
+main()
