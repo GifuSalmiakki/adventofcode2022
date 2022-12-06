8c1ec84a636b6dd54fb3bd9c57383781b17b1b82
Sisu Suominen <sisu.suominen@tuni.fi>
2022-12-06 22:19:43 +0200

    Returned solution to silver star level (groups of 4 not 14)

--- day6/day6.py
+++ day6/day6.py
@@ -8,14 +8,14 @@ def main():
 
     found = False
     for index in range(len(input)+1):
-        if index >= 13 and not found:
-            group_of_four = input[index-14:index]
+        if index >= 3 and not found:
+            group_of_four = input[index-4:index]
             unique_chars = ""
             for char in group_of_four:
                 if char not in unique_chars:
                     unique_chars += char
 
-                if len(unique_chars) == 14:
+                if len(unique_chars) == 4:
                     print(index)
                     found = True
                 else:
